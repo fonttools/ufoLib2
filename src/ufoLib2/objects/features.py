2674668dd05d13082f096a57398cdda4ea71e252
Cosimo Lupo <cosimo@anthrotype.com>
2018-10-24 18:16:45 +0100

    features: add __bool__ and __str__ methods

--- src/ufoLib2/objects/features.py
+++ src/ufoLib2/objects/features.py
@@ -4,3 +4,11 @@ import attr
 @attr.s(slots=True)
 class Features(object):
     text = attr.ib(default="", type=str)
+
+    def __bool__(self):
+        return bool(self.text)
+
+    __nonzero__ = __bool__
+
+    def __str__(self):
+        return self.text
