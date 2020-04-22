a2f2bdaef89a1a7d91192b51ee14d01dbc1ddc93
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2020-04-22 15:26:42 +0100

    Misc: type-annotate DataStore

--- src/ufoLib2/objects/misc.py
+++ src/ufoLib2/objects/misc.py
@@ -11,8 +11,6 @@ from fontTools.ufoLib import UFOReader, UFOWriter
 
 from ufoLib2.typing import Number
 
-# sentinel value to signal a "lazy" object hasn't been loaded yet
-_NOT_LOADED = object()
 
 
 BoundingBox = namedtuple("BoundingBox", "xMin yMin xMax yMax")
@@ -48,6 +46,17 @@ def _deepcopy_unlazify_attrs(self, memo):
     )
 
 
+class Placeholder:
+    """Represents a sentinel value to signal a "lazy" object hasn't been loaded yet."""
+
+
+_NOT_LOADED = Placeholder()
+
+
+# Create a generic variable that can be 'DataStore', or any subclass.
+T = TypeVar("T", bound="DataStore")
+
+
 @attr.s(auto_attribs=True, slots=True, repr=False)
 class DataStore(MutableMapping):
     """Represents the base class for ImageSet and DataSet.
@@ -56,10 +65,25 @@ class DataStore(MutableMapping):
     differ in which reader and writer methods they call.
     """
 
-    _data: Dict[str, Any] = attr.ib(factory=dict)
+    _data: Dict[str, Union[bytes, Placeholder]] = attr.ib(factory=dict)
 
-    _reader: Optional[Any] = attr.ib(default=None, init=False, repr=False, eq=False)
+    _reader: Optional[UFOReader] = attr.ib(
+        default=None, init=False, repr=False, eq=False
+    )
     _scheduledForDeletion: Set[str] = attr.ib(factory=set, init=False, repr=False)
+
+    @classmethod
+    def read(cls: Type[T], reader: UFOReader, lazy: bool = True) -> T:
+        """Instantiate the data store from a :class:`fontTools.ufoLib.UFOReader`."""
+        self = cls()
+        for fileName in cls.list_contents(reader):
+            if lazy:
+                self._data[fileName] = _NOT_LOADED
+            else:
+                self._data[fileName] = cls.read_data(reader, fileName)
+        if lazy:
+            self._reader = reader
+        return self
 
     @staticmethod
     @abstractmethod
@@ -85,19 +109,8 @@ class DataStore(MutableMapping):
         """Remove the data at filename within the store."""
         ...
 
-    @classmethod
-    def read(cls, reader, lazy=True):
-        self = cls()
-        for fileName in cls.list_contents(reader):
-            if lazy:
-                self._data[fileName] = _NOT_LOADED
-            else:
-                self._data[fileName] = cls.read_data(reader, fileName)
-        if lazy:
-            self._reader = reader
-        return self
-
-    def unlazify(self):
+    def unlazify(self) -> None:
+        """Load all data into memory."""
         for _ in self.items():
             pass
 
@@ -105,28 +118,29 @@ class DataStore(MutableMapping):
 
     # MutableMapping methods
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._data)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
         return iter(self._data)
 
-    def __getitem__(self, fileName):
-        if self._data[fileName] is _NOT_LOADED:
-            self._data[fileName] = self.read_data(self._reader, fileName)
-        return self._data[fileName]
+    def __getitem__(self, fileName: str) -> bytes:
+        data_object = self._data[fileName]
+        if isinstance(data_object, Placeholder):
+            data_object = self._data[fileName] = self.read_data(self._reader, fileName)
+        return data_object
 
-    def __setitem__(self, fileName, data):
+    def __setitem__(self, fileName: str, data: bytes) -> None:
         # should we forbid overwrite?
         self._data[fileName] = data
         if fileName in self._scheduledForDeletion:
             self._scheduledForDeletion.remove(fileName)
 
-    def __delitem__(self, fileName):
+    def __delitem__(self, fileName: str) -> None:
         del self._data[fileName]
         self._scheduledForDeletion.add(fileName)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         n = len(self._data)
         return "<{}.{} ({}) at {}>".format(
             self.__class__.__module__,
@@ -135,7 +149,8 @@ class DataStore(MutableMapping):
             hex(id(self)),
         )
 
-    def write(self, writer, saveAs=None):
+    def write(self, writer: UFOWriter, saveAs: Optional[bool] = None) -> None:
+        """Write the data store to a :class:`fontTools.ufoLib.UFOWriter`."""
         if saveAs is None:
             saveAs = self._reader is not writer
         # if in-place, remove deleted data
@@ -148,7 +163,8 @@ class DataStore(MutableMapping):
             # 1) We are saving in-place. Only write to disk what is loaded, it
             #    might be modified.
             # 2) We save elsewhere. Load all data files to write them back out.
-            if data is _NOT_LOADED:
+            # XXX: Move write_data into `if saveAs` branch to simplify code?
+            if isinstance(data, Placeholder):
                 if saveAs:
                     data = self.read_data(self._reader, fileName)
                     self._data[fileName] = data
@@ -161,7 +177,8 @@ class DataStore(MutableMapping):
             self._reader = None
 
     @property
-    def fileNames(self):
+    def fileNames(self) -> List[str]:
+        """Returns a list of filenames in the data store."""
         return list(self._data.keys())
 
 
