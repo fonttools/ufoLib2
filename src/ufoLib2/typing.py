f89ca151a902aba1ffbf171475793d59c9c55f5c
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2020-04-29 12:48:32 +0100

    Drawable: just ask for draw()
    That's what we use.

--- src/ufoLib2/typing.py
+++ src/ufoLib2/typing.py
@@ -3,7 +3,6 @@ import sys
 from typing import TypeVar, Union
 
 from fontTools.pens.basePen import AbstractPen
-from fontTools.pens.pointPen import AbstractPointPen
 
 if sys.version_info >= (3, 8):
     from typing import Protocol
@@ -21,12 +20,8 @@ PathLike = Union[str, bytes, os.PathLike]
 class Drawable(Protocol):
     """Stand-in for an object that can draw itself with a given pen.
 
-    See :mod:`fontTools.pens.basePen` and :mod:`fontTools.pens.pointPen` for an
-    introduction to pens.
+    See :mod:`fontTools.pens.basePen` for an introduction to pens.
     """
 
     def draw(self, pen: AbstractPen) -> None:
-        ...
-
-    def drawPoints(self, pointPen: AbstractPointPen) -> None:
         ...
