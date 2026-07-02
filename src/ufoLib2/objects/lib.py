a0c9d24abd1a7a33f7fae2b88c5186abecb7667c
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-07-02 11:43:54 -0500

    `PlistEncodable: TypeAlias`

--- src/ufoLib2/objects/lib.py
+++ src/ufoLib2/objects/lib.py
@@ -1,40 +1,40 @@
 from __future__ import annotations
 
-from collections.abc import Mapping
+from collections.abc import Mapping, MutableMapping, MutableSequence
 from datetime import datetime
-from functools import partial
-from typing import TYPE_CHECKING, Any, TypeAlias, TypeVar, cast, overload
+from typing import Any, TypeAlias, cast
+
+from cattrs import Converter
+from typing_extensions import TypeIs
 
 from ufoLib2.constants import DATA_LIB_KEY
 from ufoLib2.serde import serde
 
-if TYPE_CHECKING:
-
-    from cattrs import Converter
-
-# unfortunately mypy is not smart enough to support recursive types like plist...
-# PlistEncodable = Union[
-#     bool,
-#     bytes,
-#     datetime,
-#     float,
-#     int,
-#     str,
-#     Mapping[str, PlistEncodable],
-#     Sequence[PlistEncodable],
-# ]
+PlistEncodable: TypeAlias = (
+    bool
+    | bytes
+    | datetime
+    | float
+    | int
+    | str
+    | MutableMapping[str, "PlistEncodable"]
+    | MutableSequence["PlistEncodable"]
+)
 
 
-def _convert_Lib(value: Mapping[str, Any]) -> Lib:
+# TODO if `Lib(value)` returns a `dict` subclass, then `value` can be `Mapping`, otherwise is must be
+# `MutableMapping`.
+def _convert_Lib(value: Mapping[str, PlistEncodable]) -> Lib:
     return value if isinstance(value, Lib) else Lib(value)
 
 
 # getter/setter properties used by Font, Layer, Glyph
+# TODO can we make `self: Any` more specific?
 def _get_lib(self: Any) -> Lib:
     return cast(Lib, self._lib)
 
 
-def _set_lib(self: Any, value: Mapping[str, Any]) -> None:
+def _set_lib(self: Any, value: Mapping[str, PlistEncodable]) -> None:
     self._lib = _convert_Lib(value)
 
 
@@ -42,114 +42,57 @@ def _get_tempLib(self: Any) -> Lib:
     return cast(Lib, self._tempLib)
 
 
-def _set_tempLib(self: Any, value: Mapping[str, Any]) -> None:
+def _set_tempLib(self: Any, value: Mapping[str, PlistEncodable]) -> None:
     self._tempLib = _convert_Lib(value)
 
 
-def is_data_dict(value: Any) -> bool:
+def is_data_dict(value: PlistEncodable) -> TypeIs[MutableMapping[str, PlistEncodable]]:
     return (
-        isinstance(value, Mapping)
+        isinstance(value, MutableMapping)
         and "type" in value
         and value["type"] == DATA_LIB_KEY
         and "data" in value
     )
 
 
-PlistScalar: TypeAlias = bool | datetime | float | int | str
-PlistContainer: TypeAlias = (
-    Mapping[str, PlistScalar] | list[PlistScalar] | tuple[PlistScalar, ...]
-)
-PlistEncodable: TypeAlias = (
-    Mapping[str, PlistContainer] | list[PlistContainer] | tuple[PlistContainer, ...]
-)
-
-形PlistScalar = TypeVar("形PlistScalar", bool, datetime, float, int, str)
-形PlistContainer = TypeVar(
-    "形PlistContainer",
-    Mapping[str, PlistScalar],
-    list[PlistScalar],
-    tuple[PlistScalar, ...],
-)
-形PlistEncodable = TypeVar(
-    "形PlistEncodable",
-    Mapping[str, PlistContainer],
-    list[PlistContainer],
-    tuple[PlistContainer, ...],
-)
-
-
-@overload
-def _unstructure_data(value: bytes, converter: Converter) -> dict[str, PlistScalar]: ...
-
-
-@overload
-def _unstructure_data(
-    value: list[PlistEncodable], converter: Converter
-) -> list[PlistEncodable]: ...
-
-
-@overload
-def _unstructure_data(
-    value: tuple[PlistEncodable, ...], converter: Converter
-) -> list[PlistEncodable]: ...
-
-
-@overload
-def _unstructure_data(
-    value: Mapping[str, PlistEncodable], converter: Converter
-) -> dict[str, PlistEncodable]: ...
-
-
-@overload
-def _unstructure_data(value: 形PlistScalar, converter: Converter) -> 形PlistScalar: ...
-
-
-def _unstructure_data(
-    value: (
-        bytes
-        | list[PlistEncodable]
-        | tuple[PlistEncodable, ...]
-        | Mapping[str, PlistEncodable]
-        | 形PlistScalar
-    ),
-    converter: Converter,
-) -> (
-    dict[str, PlistScalar]
-    | list[PlistEncodable]
-    | dict[str, PlistEncodable]
-    | 形PlistScalar
-):
-    recurse: partial[PlistEncodable] = partial(_unstructure_data, converter=converter)
+def _unstructure_data(value: PlistEncodable, converter: Converter) -> PlistEncodable:
     if isinstance(value, bytes):
         return {"type": DATA_LIB_KEY, "data": converter.unstructure(value)}
-    elif isinstance(value, (list, tuple)):
-        return list(map(recurse, value))
-    elif isinstance(value, Mapping):
-        kk = tuple(value.keys())
-        vv = tuple(value.values())
-        rr = tuple(map(recurse, vv))
-        return dict(zip(kk, rr, strict=True))
+    elif isinstance(value, MutableSequence):
+        return list(_unstructure_data(v, converter) for v in value)
+    elif isinstance(value, MutableMapping):
+        return {k: _unstructure_data(v, converter) for k, v in value.items()}
     return value
 
 
+# NOTE "inplace" requires `Mutable*`.
 def _structure_data_inplace(
-    key: int | str, value: Any, container: Any, converter: Converter
+    key: int | str,
+    value: PlistEncodable,
+    container: MutableMapping[str, PlistEncodable] | MutableSequence[PlistEncodable],
+    converter: Converter,
 ) -> None:
-    if isinstance(value, list):
+    if isinstance(value, MutableSequence):
         for i, v in enumerate(value):
             _structure_data_inplace(i, v, value, converter)
-    elif is_data_dict(value):
+    # TODO I _feel_ like this shouldn't need 3 checks.
+    elif (
+        is_data_dict(value)
+        and isinstance(key, str)
+        and isinstance(container, MutableMapping)
+    ):
         container[key] = converter.structure(value["data"], bytes)
-    elif isinstance(value, Mapping):
+    elif isinstance(value, MutableMapping):
         for k, v in value.items():
             _structure_data_inplace(k, v, value, converter)
+    # TODO I _feel_ like `_structure_data_inplace` should "symmetrical" with `_unstructure_data`, but
+    # `_structure_data_inplace` does not have an analog to `return value`. That makes me wonder if the
+    # logic needs to be changed.
 
 
 @serde
-class Lib(dict[str, Any]):
-    def _unstructure(
-        self, converter: Converter
-    ) -> dict[str, bytes | PlistEncodable] | dict[str, PlistEncodable]:
+class Lib(dict[str, PlistEncodable]):
+    def _unstructure(self, converter: Converter) -> PlistEncodable:
         # avoid encoding if converter supports bytes natively
         test: bytes | str | object = converter.unstructure(b"\0")
         if isinstance(test, bytes):
@@ -157,16 +100,13 @@ class Lib(dict[str, Any]):
         elif not isinstance(test, str):
             raise NotImplementedError(type(test))
 
-        data: dict[str, PlistEncodable] = _unstructure_data(self, converter)
-        return data
+        return _unstructure_data(self, converter)
 
     @staticmethod
     def _structure(
-        data: Mapping[str, Any],
-        cls: type[Lib],
-        converter: Converter,
+        data: Mapping[str, PlistEncodable], cls: type[Lib], converter: Converter
     ) -> Lib:
-        self = cls(data)
+        self: Lib = cls(data)
         for k, v in self.items():
             _structure_data_inplace(k, v, self, converter)
         return self
